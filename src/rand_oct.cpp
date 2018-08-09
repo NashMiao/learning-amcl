c5fc96958b27a63262d3470b62c83f0a5a40c7ff
dingxin <wdx7266@vip.qq.com>
2018-08-09 11:30:25 +0800

    update

--- src/rand_oct.cpp
+++ src/rand_oct.cpp
@@ -22,11 +22,9 @@ void rand_oct1()
 
 void rand_oct2()
 {
-    // Crypto Strong RNG
     amcl::csprng RNG;
     std::string raw = "onchain";
     amcl::octet RAW = {0, (int)raw.size(), (char *)raw.c_str()};
-    // initialise strong RNG
     amcl::CREATE_CSPRNG(&RNG, &RAW);
     char s[512];
     amcl::octet oct = {0, sizeof(s), s};
