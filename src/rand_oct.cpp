08402f7d763b249ebfb47e9241eeddcaaf097d6b
dingxin <wdx7266@vip.qq.com>
2018-08-09 11:29:47 +0800

    update

--- src/rand_oct.cpp
+++ src/rand_oct.cpp
@@ -4,7 +4,7 @@
 #include <amcl/amcl.h>
 #include <amcl/randapi.h>
 
-int main(int argc, char const *argv[])
+void rand_oct1()
 {
     // Crypto Strong RNG
     amcl::csprng RNG;
@@ -15,7 +15,40 @@ int main(int argc, char const *argv[])
     char s[256];
     amcl::octet oct = {0, sizeof(s), s};
     amcl::OCT_rand(&oct, &RNG, 256);
+    amcl::KILL_CSPRNG(&RNG);
     amcl::OCT_output(&oct);
     std::cout << s << std::endl;
+}
+
+void rand_oct2()
+{
+    // Crypto Strong RNG
+    amcl::csprng RNG;
+    std::string raw = "onchain";
+    amcl::octet RAW = {0, (int)raw.size(), (char *)raw.c_str()};
+    // initialise strong RNG
+    amcl::CREATE_CSPRNG(&RNG, &RAW);
+    char s[512];
+    amcl::octet oct = {0, sizeof(s), s};
+    amcl::OCT_rand(&oct, &RNG, 512);
+    amcl::KILL_CSPRNG(&RNG);
+    amcl::OCT_output(&oct);
+    std::cout << s << std::endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "rand_oct1()" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    rand_oct1();
+    std::cout << "----------------------------------------------------"
+              << std::endl;
+    std::cout << "rand_oct2()" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    rand_oct2();
+    std::cout << "----------------------------------------------------"
+              << std::endl;
     return 0;
 }
