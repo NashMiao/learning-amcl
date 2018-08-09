9d34a5801d008059a18d57b02f4953e4ef1430de
dingxin <wdx7266@vip.qq.com>
2018-08-09 11:42:33 +0800

    update

--- src/rand_big.cpp
+++ src/rand_big.cpp
@@ -2,19 +2,21 @@
 #include <string>
 
 #include <amcl/amcl.h>
+#include <amcl/big_B1024_28.h>
 #include <amcl/randapi.h>
-#include <amcl/big_B1024_58.h>
-int main()
+
+int main(int argc, char const *argv[])
 {
     std::string raw = "onchain";
     amcl::octet RAW = {0, (int)raw.size(), (char *)raw.c_str()};
     amcl::csprng RNG; // Crypto Strong RNG
 
-    B1024_58::BIG num;
+    B1024_28::BIG num;
     amcl::CREATE_CSPRNG(&RNG, &RAW); // initialise strong RNG
-    B1024_58::BIG_random(num, &RNG);
+    B1024_28::BIG_random(num, &RNG);
     char *a = new char[1024];
-    B1024_58::BIG_toBytes(a, num);
+    B1024_28::BIG_toBytes(a, num);
     std::cout << a << std::endl;
+    amcl::KILL_CSPRNG(&RNG);
     return 0;
 }
